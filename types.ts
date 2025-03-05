bab094f85cb5ac1ef224ff5bdb5f6a39fbe628a9
Daniel <daanielescobar@gmail.com>
2025-03-05 21:58:11 +0000

    update library

---
+++ types.ts
@@ -0,0 +1,66 @@
+export interface Conversation {
+    id: string;
+    name: string;
+    inputs: object;
+    status: string;
+    introduction: string;
+    created_at: number;
+    updated_at: number;
+}
+
+export interface Conversations {
+    data: Conversation[];
+    limit: number; // Number of entries returned
+    has_more?: boolean; // optional property (if input exceeds system limit)
+}
+
+export interface DifyChatCompletion {
+    event: "message";
+    message_id: string;
+    conversation_id: string;
+    mode: "chat";
+    answer: string;
+    metadata: Metadata;
+    created_at: number;
+}
+
+export interface Metadata {
+    usage: Usage;
+    retriever_resources: RetrieverResource[];
+}
+
+export interface Usage {
+    prompt_tokens: number;
+    prompt_unit_price: string;
+    prompt_price_unit: string;
+    prompt_price: string;
+    completion_tokens: number;
+    completion_unit_price: string;
+    completion_price_unit: string;
+    completion_price: string;
+    total_tokens: number;
+    total_price: string;
+    currency: string;
+    latency: number;
+}
+
+export interface RetrieverResource {
+    position: number;
+    dataset_id: string;
+    dataset_name: string;
+    document_id: string;
+    document_name: string;
+    segment_id: string;
+    score: number;
+    content: string;
+}
+
+export interface SendMessageResponse {
+    answer: string,
+    conversation_id: string
+}
+
+export interface CustomerBusinessNumbers {
+    customerPhoneNumber: string,
+    businessPhoneNumber: string
+}
